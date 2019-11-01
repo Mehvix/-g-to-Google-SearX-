a2f37a05d9d6615aa25cd26da60d6cb41abc8770
Max <mehvix@protonmail.com>
2019-11-01 16:17:44 -0500

    added ddg

--- Press "g" to Google (Disroot's SearX).user.js
+++ Press "g" to Google (Disroot's SearX).user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Press "g" to Google (Disroot's SearX)
+// @name         Press "g" to Google and "d" to DDG (Disroot's SearX)
 // @version      0.1
-// @description  Press "g" to Google for Disroot's SearX
+// @description  Press "g" to Google and "d" to DDG for Disroot's SearX
 // @author       Mehvix
 // @match        https://search.disroot.org/*
 // @grant        none
@@ -14,15 +14,18 @@
         if ('input' === document.activeElement.tagName.toLowerCase()) {
             return;
         }
-        if ('g' !== event.key) {
-            return;
+        if ('d' == event.key) {
+            var q = document.getElementById('q');
+            var url = 'https://www.duckduckgo.com/search?q=' + encodeURIComponent(q.value);
+            // console.log(url);
+            document.location = url;
         }
+        if ('g' == event.key) {
 
-        var q = document.getElementById('q');
-        var url = 'https://www.google.com/search?q=' + encodeURIComponent(q.value);
-
-        // console.log(url);
-
-        document.location = url;
+            var q = document.getElementById('q');
+            var url = 'https://www.google.com/search?q=' + encodeURIComponent(q.value);
+            // console.log(url);
+            document.location = url;
+        }
     });
 })();
